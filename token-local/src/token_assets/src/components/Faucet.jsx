ebc0f1a9d1de6e5845f5cb03807fa7ff0b058309
James Lo <llotszfor12346@gmail.com>
2023-01-30 01:03:58 +0800

    first commit without overall readme

---
+++ token-local/src/token_assets/src/components/Faucet.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { token } from "../../../declarations/token";
+
+function Faucet(props) {
+  const [isDisabled, setDisable] = useState(false);
+  const [buttonText, setText] = useState("Gimme gimme");
+
+  async function handleClick(event) {
+    setDisable(true);
+    const result = await token.payOut();
+    console.log("payout: " + result);
+    setText(result);
+  }
+
+  return (
+      <div className="blue window">
+          <h2>
+              <span role="img" aria-label="tap emoji">
+                  🚰
+              </span>
+              Faucet
+          </h2>
+          <label>
+              Get your free DSpartan tokens here! Claim 10,000 SPARTAN tokens to
+              2vxsx-fae
+          </label>
+          <p className="trade-buttons">
+              <button
+                  id="btn-payout"
+                  onClick={handleClick}
+                  disabled={isDisabled}
+              >
+                  {buttonText}
+              </button>
+          </p>
+      </div>
+  );
+}
+
+export default Faucet;
